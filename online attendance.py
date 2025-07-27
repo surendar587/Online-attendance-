866fd6486bb89fa64296779fc284f529fb4261c6
surendar587 <surendarkarnati3@gmail.com>
2025-07-27 18:29:18 +0530

    Add files via upload

---
+++ online attendance.py
@@ -0,0 +1,17 @@
+import pandas as pd
+students={
+" students ID:"[ ],
+"Name":[ ],
+"subjects":[ ],
+"labs":[ ],
+"Marks":[ ],
+"Attendance"[ ]
+}
+subject_labs={
+"subjects "=["maths","physics","data structures","engnerrring drawing","Beee","lsrw","soft skills"],
+"labs"=["physics lab","ds lab"," beee lab","engnerrring drawing lab"]
+def add_students data( ):
+	student_id=input("enter student ID:")
+	name=input(" enter the student name:")
+	students["student ID" ].append(student_id)
+	
